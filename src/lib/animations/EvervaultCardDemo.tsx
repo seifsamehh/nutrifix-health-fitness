f087528ce08b49b55d8828c25835a869ca029dab
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-03-26 22:36:02 +0200

    Component Update

--- src/lib/animations/EvervaultCardDemo.tsx
+++ src/lib/animations/EvervaultCardDemo.tsx
@@ -37,5 +37,44 @@ export async function EvervaultCardDemo() {
       console.error(error);
     }
   }
-  return null;
+  return (
+    <div className="cards flex justify-center items-center min-[290px]:flex-wrap md:flex-nowrap min-[290px]:gap-6 md:gap-0">
+      {/* workout */}
+      <div className="card border border-black/[0.2] dark:border-white/[0.2] bg-background flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -right-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -right-3 dark:text-white text-black" />
+        <EvervaultCard text="workout" />
+        <h3 className="text-black dark:text-white mt-4 text-sm font-light">
+          Here we talk about the workout problem
+        </h3>
+        <Badge className="mt-4">planner</Badge>
+      </div>
+      {/* diet */}
+      <div className="card border border-black/[0.2] dark:border-white/[0.2] bg-background flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -right-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -right-3 dark:text-white text-black" />
+        <EvervaultCard text="diet" />
+        <h3 className="text-black dark:text-white mt-4 text-sm font-light">
+          Here we talk about the diet problem
+        </h3>
+        <Badge className="mt-4">planner</Badge>
+      </div>
+      {/* calories */}
+      <div className="card border border-black/[0.2] dark:border-white/[0.2] bg-background flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -left-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 -top-[13.2rem] -right-3 dark:text-white text-black" />
+        <Icon className="absolute h-6 w-6 bottom-[11.7rem] -right-3 dark:text-white text-black" />
+        <EvervaultCard text="calories" />
+        <h3 className="text-black dark:text-white mt-4 text-sm font-light">
+          Here we talk about the calories problem
+        </h3>
+        <Badge className="mt-4">estimation</Badge>
+      </div>
+    </div>
+  );
 }
